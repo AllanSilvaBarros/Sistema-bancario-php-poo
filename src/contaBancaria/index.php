8e90a5264b51d066cc035adc824796b6f84897ea
AllanSilvaBarros <allandasilvabarros58@gmail.com>
2025-09-17 21:59:45 -0300

    .

--- src/contaBancaria/index.php
+++ src/contaBancaria/index.php
@@ -8,35 +8,56 @@ class ContaBanco {
     private $saldo ;
     private $status ;
 
-    function ContaBanco($nc, $ct, $nome, $saldo, $status){
+    function ContaBanco($nc, $ct, $nome){
         $this->numConta = $nc;
         $this->contatipo = $ct;
         $this->dono = $nome;
-        $this->saldo = $saldo;
-        $this->status = $status;
+        $this->saldo = 0;
+        $this->status = false;
     }
 
-    public function abrirConta (){
+    public function abrirConta (string $ct){
+        $this->contatipo = $ct;
+        $this->status = true;
+        if ($ct = "contaCorrente") {
+            $this->saldo = 50;
+    }   elseif ($ct = "contaPoupança") {
+        $this->saldo = 150;
+    }
 
     }
 
     public function fecharConta (){
-
+        if($this->saldo > 0){
+            return "Remova o saldo da conta";
+        }elseif ($this->saldo < 0) {
+            return "Quite o debito antes de fechar a conta";
+        }else{
+            $this->status = false;
+            return "conta encerrada";
+        }
     }
 
     public function depositar (float $deposito){
+        if ($this->status = true) {
         $this->saldo += $deposito;
         return "deposito Realizado";
+        }
     }
 
     public function sacar (float $retirada){
+        if ($this->status = true) {
         $this->saldo -= $retirada;
         return "saque Realizado";
+        }
     }
 
     public function pagarTaxa (float $taxa = 27,){
+        if ($this->status = true) {
         $this->saldo -= $taxa;
         return "Pagamento Realizado";
+        }
+
     }
 
     public function getnumconta(){
